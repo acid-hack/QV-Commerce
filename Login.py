204d192a62903b546ee676c84b1b2eb265dca317
maartenelgar <40140464+maartenelgar@users.noreply.github.com>
2018-11-17 15:26:06 +0000

    Create Login.py

---
+++ Login.py
@@ -0,0 +1,24 @@
+import sql
+
+SQL = sql.SQL(acid, acid_hack, Amazon)
+
+Class Account(object):
+
+    def __init__(self):
+        self.username = None
+        self.password = None
+
+
+    def Register():
+        email = str(input('Insert Email'))
+        if "@" in str(email);
+        password = str(input('Insert Password'))
+
+        if SQL.check_login(email, password) = True;
+        #user logged in
+        else:
+        SQL.register(email, password)
+        print("account is now registered")
+        #user logged in
+        return email, password
+    
