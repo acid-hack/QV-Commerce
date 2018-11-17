9ef6c08a6dc635929819bad8f6bda3d8d2f9c680
doodspav <doods9doods@gmail.com>
2018-11-17 19:58:47 +0000

    Update voting.py

--- voting.py
+++ voting.py
@@ -1,20 +1,20 @@
-import sql
+from sql import SQL
 
-SQL = sql.SQL(acid, acid_hack, Amazon)
+sql = SQL('acid', 'acid_hack', 'Amazon')
 
 
 def voting(self, uid, pid):
 
     x = int(input("How do you score this product? "))
 
-    if x <= int( tokens_remaining() ):
-        SQL.vote(uid, pid, x^2)
+    if x <= int( sql.tokens_remaining(uid) ):
+        sql.vote(uid, pid, x^2)
 
-    while x > int( tokens_remaining(uid) ):
+    while x > int( sql.tokens_remaining(uid) ):
         x = int(input("You don't have the required number of tokens for such a score. Please try again "))
 
-        if x <= int( tokens_remaining( uid )):
-            SQL.vote ( uid, pid, x^2 )
+        if x <= int( sql.tokens_remaining( uid )):
+            sql.vote( uid, pid, x^2 )
 
             break
     return x , uid, pid
