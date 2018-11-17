751a744906bc04ca62bacb15f9ddd0c1363f62da
doodspav <doods9doods@gmail.com>
2018-11-17 15:05:43 +0000

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,3 +1,4 @@
+import bycrypt
 import MySQLdb
 
 class SQL:
@@ -11,6 +12,53 @@ class SQL:
         self._db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db, charset='utf8mb4')
         self._db.autocommit(True)
         self._cur = self._db.cursor()
+
+    def create_fresh(self):
+        """
+        Creates a fresh AccountInfo table
+        :return:
+        """
+        q = "CREATE TABLE `AccountInfo` (uid CHAR(33) UNIQUE, email VARCHAR(254) UNIQUE, passwd CHAR(128), orders INT UNSIGNED, products_voted INT UNSIGNED, tokens_remaining INT UNSIGNED, tokens_spent INT UNSIGNED, last_updated INT UNSIGNED) ENGINE=InnoDB"
+
+    def populate_acc_info(self):
+        """
+        Populates account info table with random data
+        :return:
+        """
+
+    def check_login(self, email, passwd):
+        """
+        Checks whether email and passwd are valid
+        :param email:
+        :param passwd:
+        :return bool: True if email and passwd are valid
+        """
+        q = "SELECT "
+
+    def register(self, email, passwd):
+        """
+        Registers a user
+        :param email:
+        :param passwd:
+        :return bool: False if user already exists
+        """
+        return
+    
+    def tokens_remaining(self, uid):
+        """
+        Checks how many tokens the user has remaining
+        :param uid: 
+        :return int: Tokens remaining 
+        """
+    
+    def vote(self, tokens):
+        """
+        Votes using however many tokens were passed
+        Updates products_voted, tokens_remaining, tokens_spent, and last_updated
+        Does NOT check that you have enough votes, and will raise an error if you don't 
+        :param tokens: Number of tokens to vote with
+        :return: 
+        """
 
     def close(self):
         """
