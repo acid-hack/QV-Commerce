49e38c5642f3f47ef1187745c5eb69630081c35d
Aengus Lynch <37474130+aengusl@users.noreply.github.com>
2018-11-17 17:38:05 +0000

    Update new_mean_stddv.py

--- new_mean_stddv.py
+++ new_mean_stddv.py
@@ -1,10 +1,14 @@
+from math import *
+
 def new_mean(mean , new_value , no_values ):
 
-    return (mean + new_value) / (no_values + 1)
+    return (no_values * mean + new_value) / (no_values + 1)
 
 def new_std_dv(std_dv , mean , new_value , no_values):
 
-    return std_dv + (new_value - mean) * (new_value - ( (mean + new_value) / (no_values + 1) ) )
+        V = no_values * (std_dv)**2 + (new_value - mean) * (new_value - ( (no_values * mean + new_value) / (no_values + 1) ) )
+
+        return sqrt( V / (no_values + 1))
 
 mean = new_mean(mean , new_value , no_values )
 std_dv = new_std_dv(std_dv , mean , new_value , no_values)
